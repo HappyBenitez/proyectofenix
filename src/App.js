707afd3fd533d75d28159f34bad4c0bafa6e7e22
HappyBenitez <70672432+HappyBenitez@users.noreply.github.com>
2020-09-30 20:01:05 -0500

    Add files via upload
    Software testeo de tradiciones y costumbres, mexicanas.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+render() {
+return (
+<div className="container">
+<h1 className="title">Proyecto Fenix <span aria-label="emoji" role="img">🔥</span></h1>
+<input type="text" className="new-task"/><p>
+            Hello, wellcome
+        </p>
+
+</div>
+);
+}
+}
+
+export default App;
